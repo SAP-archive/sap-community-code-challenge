13d00c68839eb1e4b2ea35b243f8e9c86b8a1f02
Daniel Wroblewski <daniel.wroblewski@sap.com>
2022-02-20 18:54:03 +0200

    Add ability to import your (or anyone's SAP avatar)

--- app/profilePic/profilePic/controller/App.controller.js
+++ app/profilePic/profilePic/controller/App.controller.js
@@ -8,9 +8,17 @@ sap.ui.define([
         "sap/ui/core/Core",
         "sap/ui/model/json/JSONModel",
         "sap/ui/Device",
-        "sap/suite/ui/commons/library"
+        "sap/suite/ui/commons/library",
+		"sap/m/Dialog",
+		"sap/m/DialogType",
+		"sap/m/Button",
+		"sap/m/ButtonType",
+		"sap/m/TextArea"
+
+
+
     ],
-    function (BaseController, MessageToast, MessageBox, oCore, JSONModel, Device, SuiteLibrary) {
+    function (BaseController, MessageToast, MessageBox, oCore, JSONModel, Device, SuiteLibrary,Dialog, DialogType,Button,ButtonType, TextArea) {
 
     return BaseController.extend("profilePic.controller.App", {
         onInit: function () {
@@ -74,17 +82,62 @@ sap.ui.define([
 
         onSaveAsPress: async function () {
             let view = this.getView()
-                let controller = view.getController()
-                let oImageEditor = view.byId("image")
-                if (!oImageEditor.getLoaded()) {
-                    MessageToast.show("Load a file first")
-                } else {
-                    oImageEditor.openSaveDialog()
-                    controller.openUrl('https://people.sap.com/', true)
-                }
+			let controller = view.getController()
+			let oImageEditor = view.byId("image")
+			if (!oImageEditor.getLoaded()) {
+				MessageToast.show("Load a file first")							
+			} else {
+				oImageEditor.openSaveDialog()
+				controller.openUrl('https://people.sap.com/', true)
+			}
 
         },
-        onImageLoaded: async function (oEvent) {
+        onGetMyAvatar: async function () {
+			if (!this.oDefaultDialog) {
+				this.oDefaultDialog = new Dialog({
+					title: "My SAP Community ID (e.g., john.smith)",
+					content : [
+						new TextArea("communityID", {
+							width: "100%",
+						})
+					
+					
+					],
+					beginButton: new Button({
+						type: ButtonType.Emphasized,
+						text: "OK",
+						press: function () {
+							this.oDefaultDialog.close();
+							this.onGetAvatar(this.oDefaultDialog);
+						}.bind(this)
+					}),
+					endButton: new Button({
+						text: "Close",
+						press: function () {
+							this.oDefaultDialog.close()
+						}.bind(this)
+					})
+				});
+
+				// to get access to the controller's model
+				this.getView().addDependent(this.oDefaultDialog);
+			}
+
+			this.oDefaultDialog.open();
+			
+			
+
+
+       },
+	   
+	   onGetAvatar: async function(oDefaultDialog) {
+           let view = this.getView()
+           let oImageEditor = view.byId("image")
+			oImageEditor.setSrc("https://avatars.services.sap.com/images/" +  oDefaultDialog.getContent()[0].getValue().trim() + ".png")
+		   
+	   },
+	   
+       onImageLoaded: async function (oEvent) {
             let view = this.getView()
                 let oImageEditor = view.byId("image")
                 oImageEditor.zoomToFit()
@@ -106,22 +159,25 @@ sap.ui.define([
 
         redirectFiori: function (url, newLang) {
             if (url.includes("sap-language")) {
+ 
                 const queryString = window.location.search;
                 const urlParams = new URLSearchParams(queryString);
                 const lang = urlParams.get('sap-language')
                     return url.replace("sap-language=" + lang, "sap-language=" + newLang)
             } else {
-                return url.replace("profilePic/", "profilepic/?sap-language=" + newLang)
+              return url.replace("profilepic/", "profilepic/?sap-language=" + newLang)
             }
         },
 
         onLanguageChange: async function (oEvent) {
             var selText = oEvent.getParameter("selectedItem").getText()
                 var selKey = oEvent.getParameter("selectedItem").getKey()
-                if (selKey != sap.ui.getCore().getConfiguration().getLanguage()) {
+               if (selKey != sap.ui.getCore().getConfiguration().getLanguage()) {
                     window.location.href = this.redirectFiori(window.location.href, selKey)
-                }
+               }
         }
 
     })
 })
+
+
